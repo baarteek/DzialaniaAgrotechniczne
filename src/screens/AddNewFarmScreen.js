e0eca6a2b5b6509f25b6a4fccff8fc044dabcf11
Bartek <bartekkrukowski822@gmail.com>
2024-05-31 17:04:17 +0200

    add farm managament screen

--- src/screens/AddNewFarmScreen.js
+++ src/screens/AddNewFarmScreen.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView } from 'react-native';
 import styles from '../style/Styles';
 
-const AddNewFarmScreen = () => {
+const AddNewFarmScreen = ({ navigation }) => {
+  const [farmName, setFarmName] = useState('');
+
+  const handleAddFarm = () => {
+    console.log(`Dodano nowe gospodarstwo: ${farmName}`);
+    setFarmName('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Add New Farm</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <Text style={styles.label}>Podaj nazwę gospodarstwa</Text>
+          <TextInput
+            style={styles.input}
+            value={farmName}
+            onChangeText={setFarmName}
+            placeholder="Nazwa gospodarstwa"
+          />
+          <TouchableOpacity style={styles.button} onPress={handleAddFarm}>
+            <Text style={styles.buttonText}>Dodaj nowe gospodarstwo</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
-
-
 
 export default AddNewFarmScreen;
--- src/style/Styles.js
+++ src/style/Styles.js
@@ -20,7 +20,38 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       fontSize: 20,
       textAlign: 'center'
-    }
+    },
+    smallButton: {
+      backgroundColor: '#098516',
+      borderRadius: 10,
+      width: 'auto',
+      padding: '3%',
+      marginVertical: '1%'
+    },
+    smallButtonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize: 16,
+      textAlign: 'center'
+    },
+    input: {
+      width: '100%',
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      marginVertical: 10,
+    },
+    label: {
+      fontSize: 18,
+      marginVertical: 10,
+    },
+    scrollContainer: {
+      flexGrow: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: '1%',
+    },
   });
 
   export default styles;
